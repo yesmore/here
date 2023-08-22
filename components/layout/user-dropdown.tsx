8d3de36cf1ad4d3439215759665be4d5e0642ea5
yesmore <3224266014@qq.com>
2023-08-22 22:51:39 +0800

    chore

--- components/layout/user-dropdown.tsx
+++ components/layout/user-dropdown.tsx
@@ -19,7 +19,7 @@ export default function UserDropdown({ session }: { session: Session }) {
       <Popover
         content={
           <div className="w-full rounded-md bg-white p-2 sm:w-56">
-            <p className="text-sm">0000000@gmail.com{email}</p>
+            <p className="text-sm">{email}</p>
             <Link
               className="relative flex w-full items-center justify-start space-x-2 rounded-md p-2 text-left text-sm transition-all duration-75 hover:bg-gray-100"
               href="/workspace"
